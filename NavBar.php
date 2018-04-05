0a5621881085f5054bd0871f74170bfd79e1c309
jon-J74 <jon.uttley@gmail.com>
2018-04-05 10:23:05 +0100

    added reset delivery at poundworld

--- NavBar.php
+++ NavBar.php
@@ -15,7 +15,9 @@
       <li class="nav-item">
         <a class="nav-link" href="PWSummary.php">Orders at BPD</a>
       </li>
-
+      <li class="nav-item">
+        <a class="nav-link" href="pwResetDel.php">Correct Status</a>
+      </li>
     </ul>
 
   </div>
